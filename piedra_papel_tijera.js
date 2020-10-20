ec4010432bf236f807b9da5f9e0e1e6bf635620a
Christian Motta <cm@castinellc.com>
2020-10-20 18:17:19 -0300

    mensajes agrupados en variables

--- piedra_papel_tijera.js
+++ piedra_papel_tijera.js
@@ -9,26 +9,31 @@ function jugar(event){
     var usuario = event.target.getAttribute('data-num');
     var computadora = aleatorio(1, 3);
 
+    // los mensajes los podes poner en variables asi no repetis codigo y lo mantenes en un solo lugar
+    var mensajeEmpataste = "<h1>empataste mi bro, para jugar de vuelta aprieta f5</h1>";
+    var mensajeGanaste = "<h1>ganaste mastodonte, para jugar de vuelta aprieta f5</h1>";
+    var mensajePerdiste = "<h1>perdiste capo,a seguir participando. Para jugar de vuelta aprieta f5</h1>";
+
     if(usuario == computadora){
-        document.write("<h1>empataste mi bro, para jugar de vuelta aprieta f5</h1>");
+        document.write(mensajeEmpataste);
     }
     if(usuario == 1 && computadora == 2){
-        document.write("<h1>perdiste capo,a seguir participando. Para jugar de vuelta aprieta f5</h1>");
+        document.write(mensajePerdiste);
     }
     else if(usuario == 1 && computadora == 3){
-        document.write("<h1>ganaste mastodonte, para jugar de vuelta aprieta f5</h1>");
+        document.write(mensajeGanaste);
     }
     if(usuario == 2 && computadora == 1){
-        document.write("<h1>ganaste mastodonte, para jugar de vuelta aprieta f5</h1>");
+        document.write(mensajeGanaste);
     }
     else if(usuario == 2 && computadora == 3){
-        document.write("<h1>perdiste capo,a seguir participando. Para jugar de vuelta aprieta f5</h1>");
+        document.write(mensajePerdiste);
     }
     if(usuario == 3 && computadora == 1){
-        document.write("<h1>perdiste capo,a seguir participando. Para jugar de vuelta aprieta f5</h1>");
+        document.write(mensajePerdiste);
     }
     else if(usuario == 3 && computadora == 2){
-        document.write("<h1>ganaste mastodonte, para jugar de vuelta aprieta f5</h1>");
+        document.write(mensajeGanaste);
     }
 }
 //la funcion con la q funciona todo
