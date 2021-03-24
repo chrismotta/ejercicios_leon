ba9a325901c15eb5d8ad59f5c2a1d48e7d2ff0ef
Christian Motta <cm@castinellc.com>
2021-03-24 19:18:40 -0300

    Ejercicio de las respuestas

---
+++ ecuaciones/ecuaciones.js
@@ -0,0 +1,28 @@
+const resultado = document.getElementById("resultado");
+resultado.innerHTML = "Esperando resultado";
+
+function getRespuestasCorrectas(
+  preguntasRespondidas,
+  puntosObtenidos,
+  valorCorrecta,
+  valorIncorrecta
+) {
+  let numeroCorrecto = "";
+
+  for (let i = 0; i <= preguntasRespondidas; i++) {
+    const _respCorrectas = preguntasRespondidas - i;
+    const _respIncorrectas = i;
+    const _total =
+      _respCorrectas * valorCorrecta + _respIncorrectas * valorIncorrecta;
+    console.log(_respCorrectas, _respIncorrectas, _total);
+
+    if (_total === puntosObtenidos) {
+      numeroCorrecto = `El participante respondio ${_respCorrectas} respuestas correctas y ${_respIncorrectas} respuestas incorrectas.`;
+      return numeroCorrecto;
+    }
+  }
+
+  return "Los valores ingresados no corresponden";
+}
+
+resultado.innerHTML = getRespuestasCorrectas(400, 550, 3, -2);
