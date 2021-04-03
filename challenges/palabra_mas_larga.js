deed16bdb96378f9e13595f8932bbbff4ee00878
Christian Motta <cm@castinellc.com>
2021-04-03 17:26:37 -0300

    challenges

---
+++ challenges/palabra_mas_larga.js
@@ -0,0 +1,11 @@
+// CONSIGNA: Crear una funcion palabraMasLarga(frase) que cuando se le pase una frase devuelva la palabra mas larga de dicha frase
+// AYUDA: Seria util convertir la frase en un array de palabras, para luego recorrer ese array de alguna manera
+// 2DO ENFOQUE: Otra opcion a recorrer el array podria ser ordenarlo
+// TEST: probar la funcion con el ejemplo dado
+
+function palabraMasLarga(frase) {
+  // codigo
+  return "";
+}
+
+console.log(palabraMasLarga("El tractorcito rojo que silbo y bufo")); // deberia imprimir "tractorcito"
