47104126a1c94f4d3ed2e5942b62780a86a99bf0
Christian Motta <cm@castinellc.com>
2021-04-04 20:54:11 -0300

    challenges

---
+++ challenges/factorial.js
@@ -0,0 +1,10 @@
+// CONSIGNA: devolver el "factorial" del numero pasado como parametro
+// EJEMPLO: el factorial de 4 es: 4 * 3 * 2 * 1 = 24
+
+function factorial(numero) {
+  // codigo
+  return "";
+}
+
+console.log(factorial(4)); // deberia imprimir 24
+console.log(factorial(8)); // deberia imprimir 40320
