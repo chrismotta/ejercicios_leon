db5a54b0506eddacc55cfb13cba4157e52fb2cbc
Christian Motta <cm@castinellc.com>
2021-04-06 11:54:10 -0300

    Palindromos

---
+++ challenges/palindromos.js
@@ -0,0 +1,13 @@
+// CONSIGNA: crear una funcion que devuelve true si el string (frase o palabra)
+// es un palíndromo válido y false si no lo es
+// AYUDA: seria util de alguna forma "invertir" el string recibido
+
+function esPalindromo(str) {
+  // codigo
+  return;
+}
+
+console.log(esPalindromo("reconocer")); // Deberia devolver TRUE
+console.log(esPalindromo("cabezota")); // Deberia devolver FALSE
+console.log(esPalindromo("Dabale arroz a la zorra el Abad")); // Deberia devolver TRUE
+console.log(esPalindromo("Dabale arroz a la zorra el Cabezota")); // Deberia devolver FALSE
