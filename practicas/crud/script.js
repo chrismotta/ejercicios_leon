395f1be06a309fae59c5d10dcb7eeeaa3ca4dfb0
Leon Rodriguez <leonrodriguez71@gmail.com>
2021-04-25 19:49:40 -0300

    objetivo 1 mejorado

---
+++ practicas/crud/script.js
@@ -0,0 +1,12 @@
+
+
+/* var comentario = [
+    {
+    _ID: 123,
+    creador: "Leon Rodriguez",
+    frechaCreacion: "2021-04-23",
+    frechaModificacion: "2021-04-23",
+    comentario: "Este es un comentario de prueba",
+    editado: false,
+    },
+]; */
