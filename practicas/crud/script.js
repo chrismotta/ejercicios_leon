7e899edc4069e6faeca13e40938370e13a6364a0
Christian Motta <cm@castinellc.com>
2021-04-27 15:45:20 -0300

    Nota Final

--- practicas/crud/script.js
+++ practicas/crud/script.js
@@ -27,25 +27,12 @@ function mostrarInfo(e) {
   e.preventDefault();
   let objeto = {
     nombre: usuario.value,
-    comentario: comentario_mensaje.value
-  }
+    comentario: comentario_mensaje.value,
+  };
   array_comentarios.push(objeto);
-  /* array_comentarios.push(
-    new crearComentarios(usuario.value, comentario_mensaje.value)
-  ); */
 
-  // Bien por haberlo resuelto, absolutamente válido... pero...
-  // al hacer una función constructora y usar 'this' y 'new' estás haciendo
-  // programación orientada a objeto, no está mal pero no es el fuerte de JS
-
-  // Te propongo otra forma más sencilla, usando el paradigma funcional
-  // Por qué no crear un objeto con el comentario, acá mismo, dentro de la funcion?
-  // const objeto = {
-  //   clave: valor,
-  //   clave: valor
-  // }
-  // Entonces podrías pushearlo al array directamente, sin necesidad de una función externa.
-  // Probalo (y no borres tu solución, dejala comentada, así la comparamos) 😉
+  // ok, no puedo ponerte un 10, por el uso del nombre "objeto" como nombre del objeto
+  // (chico literal! ponele un nombre!! nuevoComentario!!! algo!!!)
 
   console.log(array_comentarios);
 }
