66ed0a9a02d0c0e0c2b63035a206508bfbf08523
Christian Motta <cm@castinellc.com>
2021-04-27 19:04:14 -0300

    Clean up

--- practicas/crud/script.js
+++ practicas/crud/script.js
@@ -3,34 +3,16 @@ const formulario = document.getElementById("formulario");
 const usuario = document.getElementById("usuario");
 const comentario_mensaje = document.getElementById("comentario");
 
-
-
 const array_comentarios = [];
 
 let acumulador = 1;
-/* function crearComentarios(
-  nombre,
-  comentario,
-  _ID,
-  fechaCreacion,
-  fechaModificacion,
-  editado
-) {
-  this.nombre = nombre;
-  this.comentario = comentario;
-  this._ID = _ID;
-  this.fechaCreacion = fechaCreacion;
-  this.fechaModificacion = fechaModificacion;
-  this.editado = editado;
-}
- */
+
 formulario.addEventListener("submit", mostrarInfo);
 
-
-
 function mostrarInfo(e) {
+  e.preventDefault(); //esto va primero siempre
+
   let tiempo = new Date();
-  e.preventDefault();
   let comentarioNuevo = {
     nombre: usuario.value,
     comentario: comentario_mensaje.value,
@@ -41,12 +23,5 @@ function mostrarInfo(e) {
   };
   array_comentarios.push(comentarioNuevo);
 
-  // ok, no puedo ponerte un 10, por el uso del nombre "objeto" como nombre del objeto
-  // (chico literal! ponele un nombre!! nuevoComentario!!! algo!!!)
-
   console.log(array_comentarios);
 }
-
-
-
-
