7218a475cff9b4dfdc3778ecf51f3c5bc8f71675
Leon Rodriguez <leonrodriguez71@gmail.com>
2021-04-28 20:39:38 -0300

    mostrar los comentarios

--- practicas/crud/script.js
+++ practicas/crud/script.js
@@ -3,30 +3,38 @@ const formulario = document.getElementById("formulario");
 const usuario = document.getElementById("usuario");
 const comentario_mensaje = document.getElementById("comentario");
 
+const esapcioComentarios = document.getElementById("container-de-comentarios");
+
 const array_comentarios = [];
 
 let acumulador = 1;
+
+let cajaComentarios = "";
 
 formulario.addEventListener("submit", mostrarInfo);
 
 function mostrarInfo(e) {
   e.preventDefault(); //esto va primero siempre
-
   const tiempo = new Date();
 
   const fecha = `${tiempo.getDate()}/${tiempo.getMonth()+1}/${tiempo.getFullYear()}`;
   const horario = `${tiempo.getHours()}:${tiempo.getMinutes()}`;
-  const fecha_y_horario = `${fecha} - ${horario}`
+  const fechaHorario = `${fecha} - ${horario}`
 
   const comentarioNuevo = {
     nombre: usuario.value,
     comentario: comentario_mensaje.value,
     _ID: acumulador++,
-    fechaCreacion: `${fecha_y_horario}`,
-    fechaModificacion: `${fecha_y_horario}`,
+    fechaCreacion: fechaHorario,
+    fechaModificacion: fechaHorario,
     editado: false,
   };
   array_comentarios.push(comentarioNuevo);
 
   console.log(array_comentarios);
+  cajaComentarios += `<p>Nombre: ${comentarioNuevo.nombre}</p>
+  <p>Comentario: ${comentarioNuevo.comentario}</p>
+  <p>Fecha de publicacion: ${comentarioNuevo.fechaCreacion}</p><br>`;
+
+  esapcioComentarios.innerHTML = cajaComentarios;
 }
