d64bc7d4edc43cd0bf334f641028e3f01ed1d8e2
Leon Rodriguez <leonrodriguez71@gmail.com>
2021-04-29 14:08:55 -0300

    popular function hecha

--- practicas/crud/script.js
+++ practicas/crud/script.js
@@ -1,9 +1,9 @@
 const formulario = document.getElementById("formulario");
 const usuario = document.getElementById("usuario");
-const comentario_mensaje = document.getElementById("comentario");
+const comentarioMensaje = document.getElementById("comentario");
 const esapcioComentarios = document.getElementById("container-de-comentarios");
 
-const array_comentarios = [];
+const arrayComentarios = [];
 let acumulador = 1;
 let cajaComentarios = "";
 
@@ -21,15 +21,15 @@ function mostrarInfo(e) {
 
   const comentarioNuevo = {
     nombre: usuario.value,
-    comentario: comentario_mensaje.value,
+    comentario: comentarioMensaje.value,
     _ID: acumulador++,
     fechaCreacion: fechaHorario,
     fechaModificacion: fechaHorario,
     editado: false,
   };
-  array_comentarios.push(comentarioNuevo);
+  arrayComentarios.push(comentarioNuevo);
 
-  console.log(array_comentarios);
+  console.log(arrayComentarios);
 
   // armar funcion externa que:
   // limpia caja comentarios
@@ -42,4 +42,23 @@ function mostrarInfo(e) {
   // <p>Fecha de publicacion: ${comentarioNuevo.fechaCreacion}</p><br>`;
 
   // esapcioComentarios.innerHTML = cajaComentarios;
+
+
+popularComentarios();
+
+
+comentarioMensaje.value = "";
 }
+
+
+function popularComentarios(){
+  cajaComentarios = "";
+  for(i=0; arrayComentarios.length>i; i++){
+    cajaComentarios += `<p>Nombre: ${arrayComentarios[i].nombre}</p>
+    <p>Comentario: ${arrayComentarios[i].comentario}</p>
+    <p>Fecha de publicacion: ${arrayComentarios[i].fechaCreacion}</p><br>`;
+    esapcioComentarios.innerHTML = cajaComentarios;
+    console.log(arrayComentarios[i]);
+  }
+}
+
