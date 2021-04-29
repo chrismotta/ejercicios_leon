d3bff41cda1fe0fbebc328bce7076d75753dac9e
Christian Motta <cm@castinellc.com>
2021-04-28 21:12:23 -0300

    Consigna funcion popularComentarios

--- practicas/crud/script.js
+++ practicas/crud/script.js
@@ -1,25 +1,23 @@
 const formulario = document.getElementById("formulario");
-
 const usuario = document.getElementById("usuario");
 const comentario_mensaje = document.getElementById("comentario");
-
 const esapcioComentarios = document.getElementById("container-de-comentarios");
 
 const array_comentarios = [];
-
 let acumulador = 1;
-
 let cajaComentarios = "";
 
 formulario.addEventListener("submit", mostrarInfo);
 
 function mostrarInfo(e) {
   e.preventDefault(); //esto va primero siempre
-  const tiempo = new Date();
 
-  const fecha = `${tiempo.getDate()}/${tiempo.getMonth()+1}/${tiempo.getFullYear()}`;
+  const tiempo = new Date();
+  const fecha = `${tiempo.getDate()}/${
+    tiempo.getMonth() + 1
+  }/${tiempo.getFullYear()}`;
   const horario = `${tiempo.getHours()}:${tiempo.getMinutes()}`;
-  const fechaHorario = `${fecha} - ${horario}`
+  const fechaHorario = `${fecha} - ${horario}`;
 
   const comentarioNuevo = {
     nombre: usuario.value,
@@ -32,9 +30,16 @@ function mostrarInfo(e) {
   array_comentarios.push(comentarioNuevo);
 
   console.log(array_comentarios);
-  cajaComentarios += `<p>Nombre: ${comentarioNuevo.nombre}</p>
-  <p>Comentario: ${comentarioNuevo.comentario}</p>
-  <p>Fecha de publicacion: ${comentarioNuevo.fechaCreacion}</p><br>`;
 
-  esapcioComentarios.innerHTML = cajaComentarios;
+  // armar funcion externa que:
+  // limpia caja comentarios
+  // tiene un for que recorre todo el array
+  // carga los textos de toooodos los comentarios en una variable
+  // manda el texto a esapcioComentarios
+
+  // cajaComentarios += `<p>Nombre: ${comentarioNuevo.nombre}</p>
+  // <p>Comentario: ${comentarioNuevo.comentario}</p>
+  // <p>Fecha de publicacion: ${comentarioNuevo.fechaCreacion}</p><br>`;
+
+  // esapcioComentarios.innerHTML = cajaComentarios;
 }
